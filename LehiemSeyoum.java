919a4e6c907c42a1fec55db752574d5a03f56350
lehiem <lehiem.s1@gmail.com>
2026-09-04 15:22:12 -0400

    recreate the structure and open new project

--- LehiemSeyoum.java
+++
@@ -1,5 +0,0 @@
-public class LehiemSeyoum {
-    public static void main(String[] args) {
-        System.out.println("Lehiem Seyoum is on the team.");
-    }
-}
--- VeronicaMunozOspina.java
+++
@@ -1,5 +0,0 @@
-public class VeronicaMunozOspina {
-    public static void main(String[] args) {
-        System.out.println("Veronica Munoz Ospina is on the team.");
-    }
-}
