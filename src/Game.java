652eeeaa2c1dc401852a4b36adf20b1b8d3625e3
verxnic2026S <vemuos@gmail.com>
2026-09-18 18:52:15 -0400

    drafting startGame

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 // -------------------------------------------------------------------------
 /**
  * game class description
@@ -7,9 +9,13 @@
  */
 public class Game {
     // ~ Fields ................................................................
-    int answer;
+    int correctAnswer;
     int guess;
     String option;
+    
+    boolean playing = true;
+    ArrayList<Integer> correctPositions = new ArrayList<>();
+    
     InputValidator validator;
     GameMethods methods;
 
@@ -19,32 +25,61 @@ public class Game {
 
     // ----------------------------------------------------------
     /**
-     * method descp.
+     * method description.
      * 
      * @return 
      */
     public void startGame() {
-    methods = new GameMethods();
-    validator = new InputValidator(); 
         
-    boolean correctType = false;
-    boolean validEntry = false;
+        Game game = new Game();
+        methods = new GameMethods();
+        validator = new InputValidator(); 
         
-    System.out.println("Intro Message");
-    System.out.println("Rules");
+        boolean correctType = false; 
+        boolean validEntry = false;
+        
+        correctAnswer = methods.generateNumber();
     
-    String userInput = promptGuess();
+        System.out.println("Intro Message");
+        System.out.println("Rules");
     
-    while (!correctType || !validEntry) {
-    correctType = isInteger();
-    validEntry = isValidGuess();
+        String userInput = promptGuess();
+        
+        correctType = validator.isInteger(userInput);
+        validEntry = validator.isValidGuess(userInput);
+        
+        while (!correctType || !validEntry) {
     
-    if (!correctType) {
-    System.out.println("");
-    userInput = promptGuess();
-    }
-    
-    else 
+            if (!correctType) {
+                System.out.println("");
+             }
+            
+            if (!validEntry) {
+                System.out.println("");
+            }
+            
+            userInput = promptGuess();
+            correctType = validator.isInteger(userInput);
+            validEntry = validator.isValidGuess(userInput);
+        }
+        
+        guess = Integer.parseInt(userInput);
+        
+        boolean winner = methods.isCorrect(guess, correctAnswer);
+        
+        while(playing) {
+            if (winner) {
+                System.out.println("win message");
+            }
+            else{
+               correctPositions = methods.getCorrectPositions();
+               System.out.println("");
+               
+               option = game.showOptions();
+            } 
+                }
+        }
+        
     
     
     }
