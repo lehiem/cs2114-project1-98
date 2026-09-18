3a24cc16265fc9018114c8e8ca0e5f1adce367cf
verxnic2026S <vemuos@gmail.com>
2026-09-18 14:47:05 -0400

    field variables declaration for game class

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,18 @@
+// -------------------------------------------------------------------------
+/**
+ * game class description
+ *
+ * @author 
+ * @version 
+ */
 public class Game {
     //~ Fields ................................................................
-
+    int answer;
+    int guess; 
+    String option;
+    InputValidator validator;
+    GameMethods methods;
+    
     //~ Constructors ..........................................................
 
     //~Public  Methods ........................................................
