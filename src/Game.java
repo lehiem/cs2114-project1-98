b73560be3c61acd1097853203e0aea15440e2918
verxnic2026S <vemuos@gmail.com>
2026-09-18 15:32:04 -0400

    game,.java changes

--- src/Game.java
+++ src/Game.java
@@ -2,19 +2,73 @@
 /**
  * game class description
  *
- * @author 
- * @version 
+ * @author
+ * @version
  */
 public class Game {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
     int answer;
-    int guess; 
+    int guess;
     String option;
     InputValidator validator;
     GameMethods methods;
-    
-    //~ Constructors ..........................................................
 
-    //~Public  Methods ........................................................
+    // ~ Constructors ..........................................................
+
+    // ~Public Methods ........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * method descp.
+     * 
+     * @return 
+     */
+    public void startGame() {
+    methods = new GameMethods();
+    validator = new InputValidator(); 
+        
+    boolean correctType = false;
+    boolean validEntry = false;
+        
+    System.out.println("Intro Message");
+    System.out.println("Rules");
+    
+    String userInput = promptGuess();
+    
+    while (!correctType || !validEntry) {
+    correctType = isInteger();
+    validEntry = isValidGuess();
+    
+    if (!correctType) {
+    System.out.println("");
+    userInput = promptGuess();
+    }
+    
+    else 
+    
+    
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * method descp.
+     * 
+     * @return
+     */
+    public String promptGuess() {
+
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * method descp.
+     * 
+     * @return
+     */
+    public String showOptions() {
+
+    }
 
 }
