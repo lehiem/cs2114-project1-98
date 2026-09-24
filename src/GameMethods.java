a4c021a4eabff22829ec7c545f36adb9a798b5f4
justisphillips <justisphillips6@gmail.com>
2026-09-23 20:02:59 -0400

    Update GameMethods.java
    Set up the methods and parameters.

--- src/GameMethods.java
+++ src/GameMethods.java
@@ -1,8 +1,67 @@
-public class GameMethods {
-    //~ Fields ................................................................
+import java.util.ArrayList;
+import java.util.Random;
 
-    //~ Constructors ..........................................................
+// -------------------------------------------------------------------------
+/**
+ * Does the calculations and operations used in the game.
+ *
+ * @author justisp
+ * @version 09.23.2026
+ */
+public class GameMethods
+{
+    // ~ Fields ................................................................
 
-    //~Public  Methods ........................................................
+    private Random random;
 
+    // ~ Constructors ..........................................................
+
+    public GameMethods()
+    {
+        random = new Random();
+    }
+
+    // ~Public Methods ........................................................
+
+
+    public int generateNumber()
+    {
+        return
+    }
+
+
+    public boolean isCorrect(int guess, int answer)
+    {
+        return
+    }
+
+
+    public ArrayList<Integer> getCorrectPositions(int guess, int answer)
+    {
+        return
+    }
+
+
+    public int getFirstDigit(int answer)
+    {
+        return 
+    }
+
+
+    public int getLastDigit(int answer)
+    {
+        return
+    }
+
+
+    public int getDigitsSum(int answer)
+    {
+        return
+    }
+
+
+    public String giveUp(int answer)
+    {
+        return
+    }
 }
